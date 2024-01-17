cf2456a02e7b38692343d5957440334cacef637c
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2024-01-17 13:06:34 +0300

    completed lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
+greet_programmer()
 def greet(name):
-    pass
-
+    print(f"Hello, {name}!")
+    
+user_name = "Chacha"
+greet(user_name)
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
+programmer_name = "Peter"
+greet_with_default(programmer_name)
+greet_with_default()
 def add(num1, num2):
-    pass
+    return num1 + num2
+result = add(2, 2)
+print(result)
 
 def halve(number):
-    pass
+    return number / 2
+answer = halve(10)
+print(answer)
